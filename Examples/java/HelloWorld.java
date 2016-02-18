4f9266cf282c58c54341b5ca7ab65bece40686a9
Steve Clement <steve@localhost.lu>
2016-02-18 16:33:59 +0100

    - Added HelloWorld java example

---
+++ Examples/java/HelloWorld.java
@@ -0,0 +1,12 @@
+// public == access modifier -> class is accessible to the entire application
+// name of classes have per definition an upper-case letter.
+public class HelloWorld {
+  // static vs. member methods of the class NOT an instance of the class
+  // void == means doesn't return a value
+  // main == main method (required) that gets run automatically (per default)
+  // name of method (aka functions, but in OO talk referred to as methods) + arguments = Method signature
+  // main method signature receives an array of string values
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
